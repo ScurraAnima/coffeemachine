05a3b02d00671af370e24412958fe6f6804d88f0
Asakura <Asakura-MBA>
2025-03-28 18:04:11 +0100

    Added calculator for coffee. Also simple checks and prefilled variables.

--- main.py
+++ main.py
@@ -21,14 +21,28 @@ money = 0
 
 
 
-def coins():
+def coins(x):
+    global money
+    dime = 0
+    nickle = 0
+    quarter = 0
+    pennie = 0 
+    
+    print("Please insert coins.")
     quarter = float(input("How many quarters?: ")) * 0.25
     dime = float(input("How many dimes?: ")) * 0.10
     nickle = float(input("How many nickles?: ")) * 0.05
-    pennie = float(input("How many pennies?: ")) * 0.01  
+    pennie = float(input("How many pennies?: ")) * 0.01
+    total = sum([quarter,dime,nickle,pennie])   
+    if total < x:
+        print(f"Sorry that's not enough money. You entered {round(total,2)}, but need {round(x - total,2)} more. Money refunded.")
+    else:
+        money += x
+        print(f"Here is ${round(total - x,2)} in change.")
     
 
 def coffee_order(order):
+    global water, milk, coffee, money  # Declare these as global
     match order:
         case "espresso":
             print("You ordered an Espresso ☕")
@@ -37,19 +51,42 @@ def coffee_order(order):
                 print("Sorry not enough water")
             elif MENU["espresso"]["ingredients"]["coffee"] > coffee:
                 print("Sorry not enough water")
-            elif MENU["espresso"]["ingredients"]["milk"] > milk:
-                print("Sorry not enough water")
             else:
-                coins()
+                coins(MENU["espresso"]["cost"])
                 water -= MENU["espresso"]["ingredients"]["water"]
-                milk -= MENU["espresso"]["ingredients"]["milk"]
                 coffee -= MENU["espresso"]["ingredients"]["coffee"]
                 
             
         case "latte":
             print("You ordered a Latte 🥛☕")
+            
+            if MENU["latte"]["ingredients"]["water"] > water:
+                print("Sorry not enough water")
+            elif MENU["latte"]["ingredients"]["milk"] > milk:
+                print("Sorry not enough water")
+            elif MENU["latte"]["ingredients"]["coffee"] > coffee:
+                print("Sorry not enough water")
+            else:
+                coins(MENU["latte"]["cost"])
+                water -= MENU["latte"]["ingredients"]["water"]
+                milk -= MENU["latte"]["ingredients"]["milk"]
+                coffee -= MENU["latte"]["ingredients"]["coffee"]
+                              
         case "cappuccino":
             print("You ordered a Cappuccino ☁☕")
+            
+            if MENU["cappuccino"]["ingredients"]["water"] > water:
+                print("Sorry not enough water")
+            elif MENU["cappuccino"]["ingredients"]["milk"] > milk:
+                print("Sorry not enough water")
+            elif MENU["cappuccino"]["ingredients"]["coffee"] > coffee:
+                print("Sorry not enough water")
+            else:
+                coins(MENU["cappuccino"]["cost"])
+                water -= MENU["cappuccino"]["ingredients"]["water"]
+                milk -= MENU["cappuccino"]["ingredients"]["milk"]
+                coffee -= MENU["cappuccino"]["ingredients"]["coffee"]
+            
         case "off":
             print("Shutting down... 📴")
             exit()
